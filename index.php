0b31cb31856dd6c9910da639f6f725331ba09162
mas <m.yoshida553@gmail.com>
2024-12-03 02:26:53 +0900

    追加

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <form action="login.php" method="post">
+    <div>
+      <label for="lid">ログインID</label>
+      <input type="text" name="lid" id="lid">
+    </div>
+    <div>
+      <label for="lpw">ログインPW</label>
+      <input type="text" name="lpw" id="lpw">
+    </div>
+    <input type="submit" value="ログイン">
+  </form>
+</body>
+
+</html>
